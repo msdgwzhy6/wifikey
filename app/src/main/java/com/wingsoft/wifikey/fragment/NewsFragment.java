1a6d04b9adb56426daf5169ad3102ed4203890c1
wing <615555755@qq.com>
2015-07-07 23:14:29 +0800

    加入资讯页

--- app/src/main/java/com/wingsoft/wifikey/fragment/NewsFragment.java
+++ app/src/main/java/com/wingsoft/wifikey/fragment/NewsFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.wingsoft.wifikey.R;
 
@@ -12,10 +14,22 @@ import com.wingsoft.wifikey.R;
  * Created by wing on 15/7/6.
  */
 public class NewsFragment extends Fragment{
+    private WebView _WebView;
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceBundle){
         View view = inflater.inflate(R.layout.fragment_news,container,false);
+        _WebView = (WebView)view.findViewById(R.id.webview);
+        _WebView.getSettings().setJavaScriptEnabled(true);
+        _WebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view ,String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        _WebView.loadUrl("http://m.baidu.com/news");
         return view;
 
     }
+
 }
